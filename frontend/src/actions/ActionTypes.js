15af6db9ee4c531adedb9a9d1d8928f7dfd8ce8d
Chris Dunkel <chris@careerfairplus.com>
2018-05-22 20:33:41 -0400

    Adds action type definitions for all possible server actions.

---
+++ frontend/src/actions/ActionTypes.js
@@ -0,0 +1,28 @@
+
+/**
+ * Post Actions
+ */
+export const FETCH_POSTS              = 'FETCH_POSTS';
+export const FETCH_POSTS_BY_CATEGORY  = 'FETCH_POSTS_BY_CATEGORY';
+export const ADD_POST                 = 'ADD_POST';
+export const FETCH_POST_DETAILS       = 'FETCH_POST_DETAILS';
+export const UPVOTE_POST              = 'UPVOTE_POST';
+export const DOWNVOTE_POST            = 'DOWNVOTE_POST';
+export const EDIT_POST                = 'EDIT_POST';
+export const DELETE_POST              = 'DELETE_POST';
+
+/**
+ * Comment Actions
+ */
+export const FETCH_COMMENTS_FOR_POST  = 'FETCH_COMMENTS_FOR_POST';
+export const ADD_COMMENT_TO_POST      = 'ADD_COMMENT_TO_POST';
+export const FETCH_COMMENT_DETAILS    = 'FETCH_COMMENT_DETAILS';
+export const UPVOTE_COMMENT           = 'UPVOTE_COMMENT';
+export const DOWNVOTE_COMMENT         = 'DOWNVOTE_COMMENT';
+export const EDIT_COMMENT             = 'EDIT_COMMENT';
+export const DELETE_COMMENT           = 'DELETE_COMMENT';
+
+/**
+ * Category Actions
+ */
+export const FETCH_CATEGORIES         = 'FETCH_CATEGORIES';
